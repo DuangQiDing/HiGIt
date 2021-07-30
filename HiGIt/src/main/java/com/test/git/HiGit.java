be130a44a839e360b7146355fb18044e635151a4
DuanQiDing <1011303747@qq.com>
2021-07-30 11:41:29 +0800

    更新2

--- HiGIt/src/main/java/com/test/git/HiGit.java
+++ HiGIt/src/main/java/com/test/git/HiGit.java
@@ -4,5 +4,6 @@ public class HiGit {
     public static void main(String[] args) {
         System.out.println("hello Git!");
         System.out.println("hi git");
+        System.out.println("hi git2");
     }
 }
